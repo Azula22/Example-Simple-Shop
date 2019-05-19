fbc083ce2151101735c7a7c42cba7bdd8c827de5
Azula22 <azulazulana@gmail.com>
2019-05-19 21:17:40 +0300

    Fix errors so they don't have stack trace for sure

--- src/main/scala/com/example/shop/scala/models/errors/LogicError.scala
+++ src/main/scala/com/example/shop/scala/models/errors/LogicError.scala
@@ -2,15 +2,13 @@ package com.example.shop.scala.models.errors
 
 import java.util.UUID
 
-sealed trait LogicError extends Exception {
-  def msg: String
-  override def setStackTrace(stackTraceElements: Array[StackTraceElement]): Unit = {} //for faster work
+import scala.util.control.NoStackTrace
+
+sealed class LogicError(msg: String) extends NoStackTrace {
   override def getMessage: String = msg
 }
 
-case class NoProduct(id: UUID) extends LogicError { def msg = s"Product with id ${id.toString} not found"}
-case class NotEnoughOfProduct(id: UUID, amount: Int) extends LogicError {
-  def msg = s"Max number of product with $id to buy is $amount"
-}
-case class NoUser(email: String) extends LogicError { def msg = s"User with email $email not found"}
-case class NoItemsToBuy(userId: String) extends LogicError { def msg = s"No items in a cart to buy for $userId"}
+case class NoProduct(id: UUID) extends LogicError(s"Product with id ${id.toString} not found")
+case class NotEnoughOfProduct(id: UUID, amount: Int) extends LogicError(s"Max number of product with $id to buy is $amount")
+case class NoUser(email: String) extends LogicError(s"User with email $email not found")
+case class NoItemsToBuy(userId: String) extends LogicError(s"No items in a cart to buy for $userId")
